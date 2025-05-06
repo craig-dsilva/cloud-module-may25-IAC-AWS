227abe7fb0e61778f73b0ae6ec7ec33d34abd282
Craig D'Silva <craig.s.dsilva@gmail.com>
2025-05-06 10:42:49 +0100

    Initialise

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "cloud-module-may25-tf-state"
+    key     = "terraform.tfstate"
+    region  = "eu-west-1"
+    profile = "craigcyf"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
