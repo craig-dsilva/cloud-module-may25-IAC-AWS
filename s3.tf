af0acecd7bd77c819f27084f56852c3adb56a715
Craig D'Silva <craig.s.dsilva@gmail.com>
2025-05-06 17:37:31 +0100

    s3 website config

--- s3.tf
+++ s3.tf
@@ -1,3 +1,15 @@
 resource "aws_s3_bucket" "frontend" {
   bucket = "craig-cyf-hotel-frontend"
 }
+
+resource "aws_s3_bucket_website_configuration" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
