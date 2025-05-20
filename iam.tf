b8d058c0c86a9b0e48ca17d532df8559c2824e4c
Craig D'Silva <craig.s.dsilva@gmail.com>
2025-05-20 17:32:56 +0100

    Backend Pipeline

--- iam.tf
+++ iam.tf
@@ -23,3 +23,25 @@ resource "aws_iam_user_policy" "frontend-pipeline-policy" {
     }]
   })
 }
+
+resource "aws_iam_user" "backend-pipeline" {
+  name = "backend-pipeline"
+}
+
+resource "aws_iam_user_policy" "backend-pipeline-policy" {
+  name = "backend-pipeline-policy"
+  user = aws_iam_user.backend-pipeline.name
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:RevokeSecurityGroupIngress",
+          "ec2:AuthorizeSecurityGroupIngress"
+        ],
+        "Resource" : aws_security_group.hotel-server.arn
+      }
+    ]
+  })
+}
