8dccab92367825e24bf4b7322ff9605e55b9f414
Craig D'Silva <craig.s.dsilva@gmail.com>
2025-05-26 15:47:06 +0100

    RDS

--- variables.tf
+++ variables.tf
@@ -7,3 +7,18 @@ variable "home_ip" {
   type      = string
   sensitive = true
 }
+
+variable "db_username" {
+  type = string
+  sensitive = true
+}
+
+variable "db_password" {
+  type = string
+  sensitive = true
+}
+
+variable "ec2_ip" {
+  type = string
+  sensitive = true
+}
